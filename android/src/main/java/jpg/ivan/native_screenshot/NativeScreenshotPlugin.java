2c49d80388b631ad34a9d518053015f2bd83dcbb
Adriel Venancio Bomfim <adriel@polkadex.trade>
2023-06-14 13:58:29 -0300

    fix: includes hashmap import

--- android/src/main/java/jpg/ivan/native_screenshot/NativeScreenshotPlugin.java
+++ android/src/main/java/jpg/ivan/native_screenshot/NativeScreenshotPlugin.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
